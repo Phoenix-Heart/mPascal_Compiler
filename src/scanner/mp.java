5d00ac765295923f6f00ad9ab9a9ba67622c2a5d
Chrysallis <lady_aviendha@hotmail.com>
2015-02-13 14:26:04 -0700

    bug fix in Identifier FSM

--- src/scanner/mp.java
+++ src/scanner/mp.java
@@ -18,6 +18,7 @@ public class mp {
             while(token!= Token.MP_EOF) {
                 token = s.getToken();
                 tokens.add(token);
+                System.out.println("Found token "+ token.name() +" at ( "+s.getLemexe()+" )");
             }
         }
         else {
